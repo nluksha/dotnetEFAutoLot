9a39e84d983ae978d94900a6808acce7e7dcd2d8
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-23 10:27:30 +0300

    Add migrations

---
+++ DotNetEFAutoLot/DotNetEFAutoLot.DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace DotNetEFAutoLot.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DotNetEFAutoLot.DAL.EF.AutoLotEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DotNetEFAutoLot.DAL.EF.AutoLotEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
